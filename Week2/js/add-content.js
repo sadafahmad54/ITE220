fb2f98324f243a1ae81233420e5f14360749767c
Sadaf Ahmad <you@example.comsadafahmad54@gmail.com>
2015-11-23 10:15:14 +0700

    Added shopping content to week 02 version 01

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,1 +1,11 @@
-document.write("Hello Week");
+var price;
+var quantity;
+var total;
+ price= 100;
+ quantity= 2;
+ total= price*quantity;
+
+//document.write("Total is " + total);
+
+var totalPriceEle = document.getElementById("totalPrice");
+totalPriceEle.textContent ="$" + total;

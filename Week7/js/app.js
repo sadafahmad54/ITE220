d13438b4388b8ae68595cd2b644bc2c0a946b104
Sadaf Ahmad <sadafahmad54@gmail.com>
2016-01-10 11:56:36 +0700

    temprature eror

---
+++ Week7/js/app.js
@@ -0,0 +1,23 @@
+//Sadaf Ahmad 
+$(function(){
+	$("a").on("click",function(e){
+
+		e.preventDefault();
+		//alert(this.text);
+		var city=this.text.toLowerCase();
+		$.ajax({
+			type:'GET',
+			url:'http://api.openweathermap.org/data/2.5/weather?q='+ city +' &appid=2de143494c0b295cca9337e1e96b00e0' ,
+			success: function(data){
+				//console.log(data.name);
+				$("#container").html(data.name +":" + data.main.temp);
+			}
+
+		});
+	});
+});
+
+
+
+
+
